bdb03e9658e717b31ab4fefe8f5c82220965b943
Theo Diamantidis <tdiam@posteo.net>
2025-08-12 22:41:14 +0100

    dev: add tests; add package.json

--- src/slack.js
+++ src/slack.js
@@ -1,5 +1,5 @@
 import { html, render } from 'lit-html';
-import { unsafeHTML } from 'lit-html/directives/unsafe-html';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 
 import { HTMLParser } from './htmlparser.js';
 
